6eae82587f332e7f78d9df9a4f62c1821604e395
SergioIznaolaDeveloper <sergioiznaola@gmail.com>
2022-02-25 09:53:45 +0100

    second_0_fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Crear mapa
 
 const map = L.map("map").setView(
   [34.054131, -118.233482] /*cordenadas*/,
@@ -23,7 +24,6 @@ L.tileLayer(
 
 async function getCars() {
   try {
-
     let response = await fetch(
       "https://api.metro.net/agencies/lametro/vehicles/"
     );
@@ -39,86 +39,42 @@ async function getCars() {
         radius: 50,
       }).addTo(map);
       let id = data.items[i].id;
-      marker.bindPopup("ID - " + id)
-      setTimeout(()=>marker.remove(),5000);
+      marker.bindPopup("ID - " + id);
+      setTimeout(() => marker.remove(), 5000);
     }
   } catch (error) {
     console.log(`ERROR Error: ${error.stack}`);
   }
 }
-getCars()
-setInterval(()=>getCars(),5000)
-
+getCars();//segundo 0
+setInterval(() => getCars(), 5000);
 
 async function getRail() {
-    try {
-      let response = await fetch(
-        "https://api.metro.net/agencies/lametro-rail/vehicles/"
-      );
-      let data = await response.json();
-  
-      for (i = 0; i < data.items.length; i++) {
-        let latitud = data.items[i].latitude;
-        let longitud = data.items[i].longitude;
-        let circle = L.circle([latitud, longitud], {
-              color: "white",
-              fillColor: "purple",
-              fillOpacity: 0.8,
-              radius: 50,
-            }).addTo(map);
-        let id = data.items[i].id;
-        circle.bindPopup("ID - " + id)
-        setTimeout(()=>circle.remove(),5000);
-      }
-    } catch (error) {
-      console.log(`ERROR Error: ${error.stack}`);
+  try {
+    let response = await fetch(
+      "https://api.metro.net/agencies/lametro-rail/vehicles/"
+    );
+    let data = await response.json();
+
+    for (i = 0; i < data.items.length; i++) {
+      let latitud = data.items[i].latitude;
+      let longitud = data.items[i].longitude;
+      let circle = L.circle([latitud, longitud], {
+        color: "white",
+        fillColor: "purple",
+        fillOpacity: 0.8,
+        radius: 50,
+      }).addTo(map);
+      let id = data.items[i].id;
+      circle.bindPopup("ID - " + id);
+      setTimeout(() => circle.remove(), 5000);
     }
+  } catch (error) {
+    console.log(`ERROR Error: ${error.stack}`);
   }
+}
 
-setInterval(()=>getRail(),5000)
 
+getRail();//segundo 0
+setInterval(() => getRail(), 5000);
 
-/*Marcador*/
-// var marker = L.marker([40.6159, -4.001]).addTo(map);
-
-/*Círculo*/
-// var circle = L.circle([40.6159, -4.0011], {
-//   color: "white",
-//   fillColor: "purple",
-//   fillOpacity: 0.2,
-//   radius: 1500,
-// }).addTo(map);
-
-/*Polígono*/
-
-// var polygon = L.polygon([
-//   [40.62, -4.002],
-//   [40.61, -4.001],
-//   [40.6, -4.003],
-// ]).addTo(map);
-
-/*popups*/
-// marker
-//   .bindPopup("<b>Aqui cerquita estoy")
-//   .openPopup(); /*sale por defecto el popup*/
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-/*trabajar con eventos = cordenadas con click*/
-
-// function onMapClick(e) {
-//     alert("You clicked the map at " + e.latlng);
-// }
-
-// map.on('click', onMapClick);
-
-/*popup al click*/
-
-// const popup = L.popup();
-// function onMapClick(e) {
-//   popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at " + e.latlng.toString())
-//     .openOn(map);
-// }
-// map.on("click", onMapClick);
